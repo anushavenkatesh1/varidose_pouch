a222efd1dc51e0a8f2babe222a557edbe87d41d3
Gowthami <103192675+Gowthamijain29@users.noreply.github.com>
2023-11-27 13:56:15 +0530

    Update PCBcode.ino
    In the previous version of the code, the RTC part of the code wasn't working as desired but now the error has been fixed.

--- Version2/PCBcode.ino
+++ Version2/PCBcode.ino
@@ -2,74 +2,120 @@
 #include "BluetoothSerial.h"
 #include "SoundData.h"
 #include "XT_DAC_Audio.h"
-#include <Arduino.h>
-#include <RTClib.h> 
+#include <Arduino.h>  
+#include <Wire.h>
+#include <RTClib.h>
+           
+BluetoothSerial SerialBT;            // Bluetooth Serial object
+RTC_DS3231 rtc;                     
+  
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-RTC_DS3231 rtc;
-
-BluetoothSerial SerialBT;   // Bluetooth Serial object
-
-const int sensorPin = 13;   // the number of the pushbutton pin
-const int ledPin = 14;    // the number of the LED pin GREEN
-const int ledPin1 = 27;    // the number of the LED pin BLUE
-const int eepromAddress = 0;  // EEPROM address to store the value
-
-int flag = 1;
+const int sensorPin = 13;          // touch sensor 
+const int ledPin = 27;           //  LED pin blue
+const int ledPin1 = 14;         //  LED pin green
+const int ledPin2 = 12;         //  LED pin BLUE
+const int eepromAddress = 0;   // EEPROM address to store the value
 
 XT_Wav_Class ForceWithYou(Force);   // create an object of type XT_Wav_Class that is used by
-                   // the dac audio class (below), passing wav data as parameter.
+                                   // the dac audio class (below), passing wav data as parameter.
 
 XT_DAC_Audio_Class DacAudio(25, 0);  // Create the main player class object.
-// Use GPIO 25, one of the 2 DAC pins and timer 0
+                                    // Use GPIO 25, one of the 2 DAC pins and timer 0
 
-uint32_t DemoCounter = 0;       // Just a counter to use in the serial monitor
+uint32_t DemoCounter = 0;       // Just a counter to use in the serial monitor variables will change
+int buttonState = 0;           // variable for reading the pushbutton status
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+#define lightsensor  36        // ESP32 pin GPIO36 (ADC0) connected to light sensor
+#define analogthreshold  600   //threshold for light intensity
 
 void setup() 
-{ Serial.begin(115200);
- SerialBT.begin("ESP32_BT");    // Initialize Bluetooth with the device name "ESP32_BT"
+{ 
+ Serial.begin(115200);
+ SerialBT.begin("ESP32");    // Initialize Bluetooth with the device name "ESP32"
  pinMode(ledPin,OUTPUT);
  pinMode(ledPin1,OUTPUT);
+ pinMode(ledPin2,OUTPUT);
  pinMode(sensorPin, INPUT);
  int storedValue = EEPROM.read(eepromAddress);
- Serial.print("Stored Value: ");
- Serial.println(storedValue);
-  rtc.begin(); // Initialize the RTC
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Set the RTC to the current date and time
-}
+ //Serial.print("Stored Value: ");
+ //Serial.println(storedValue);
+ 
+ if (!rtc.begin())
+  {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
 
-void loop() { 
-  DateTime now = rtc.now(); // Get the current time from the RTC
-  int minutes = now.minute(); // Get the minutes component of the current time
+  // Uncomment the following line to set the RTC to the current date and time
+  rtc.adjust(DateTime(__DATE__, __TIME__));
+ }
 
-  // For every 10 minutes
-  if (minutes % 2 == 0) {
- // read the state of the pushbutton value:
+void loop() 
+{ 
+ digitalWrite(ledPin, HIGH);    // setting the rgb off initially , rgb common anode
+ digitalWrite(ledPin1,HIGH);
+ digitalWrite(ledPin2, HIGH); 
+ 
+ DateTime now = rtc.now();
+ int analogValue = analogRead(lightsensor);   // read the value on analog pin
+ //Serial.println(analogValue);
  buttonState = digitalRead(sensorPin);  
- if (buttonState == LOW) 
- { int valueToStore = 42;
+ 
+ if (now.minute() % 2 == 0 && analogValue < analogthreshold && buttonState == LOW) 
+ {
+  int valueToStore = 42;
   EEPROM.write(eepromAddress, valueToStore);
   EEPROM.commit();
- }
- else { // turn LED off:
-  digitalWrite(ledPin, LOW);
-  digitalWrite(ledPin1, HIGH);
-  SerialBT.println("Alarm OFF");   // Send a Bluetooth message
-  Serial.println("Alarm OFF");    // display on serial moniotr
-  delay(1000);
-  flag = 0;
-  }
- if (flag == 1) {
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin, HIGH);         //blue led is off
+  digitalWrite(ledPin1, LOW);         // green led is on  
+  Serial.print(now.hour(), DEC);
+  Serial.print(":");
+  Serial.print(now.minute(), DEC);
+  Serial.print(":");
+  Serial.print(now.second(), DEC);
+  Serial.print(":");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.println(" ");
+  Serial.print(now.day(), DEC);
+  Serial.print("-");
+  Serial.print(now.month(), DEC);
+  Serial.print("-");
+  Serial.println(now.year(), DEC);
+  Serial.println("Alarm ON");      // display on serial monitor
+  //delay(1000);
   SerialBT.println("Alarm ON");     // Send a Bluetooth message
-  Serial.println("Alarm ON");      // display on serial moniotr
-  delay(20);
-  DacAudio.FillBuffer();         // Fill the sound buffer with data
-  if (ForceWithYou.Playing == false)   // if not playing,
-  DacAudio.Play(&ForceWithYou);     // play it, this will cause it to repeat and repeat...  
+ {sound();}
   }
+
+if( analogValue > analogthreshold && buttonState == HIGH ) //|| buttonState == HIGH || analogValue > analogthreshold 
+ { 
+  digitalWrite(ledPin, LOW);            // blue led is on
+  digitalWrite(ledPin1, HIGH);          //green led is off
+  Serial.print(now.hour(), DEC);
+  Serial.print(":");
+  Serial.print(now.minute(), DEC);
+  Serial.print(":");
+  Serial.print(now.second(), DEC);
+  Serial.print(":");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.println(" ");
+  Serial.print(now.day(), DEC);
+  Serial.print("-");
+  Serial.print(now.month(), DEC);
+  Serial.print("-");
+  Serial.println(now.year(), DEC);
+  Serial.println("Alarm OFF");      // display on serial monitor
+  delay(1000);
+  SerialBT.println("Alarm OFF");   // Send a Bluetooth message
+  delay(59000);
+ }
 }
+
+void sound()
+{
+  //delay(20);
+  DacAudio.FillBuffer();                // Fill the sound buffer with data
+  if (ForceWithYou.Playing == false)    // if not playing,
+  DacAudio.Play(&ForceWithYou);         // play it, this will cause it to repeat and repeat...  
 }
