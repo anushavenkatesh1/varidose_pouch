9e887f59043292976b24219a99836d4b562d1f57
Gowthami <103192675+Gowthamijain29@users.noreply.github.com>
2023-12-21 21:47:01 +0530

    Update PCBcode.ino
     In the code provided below, oled(display), ldr and reed switch have been interfaced with the esp32 , 
     where the alarm gets triggered for every 2 min and at the time of alarm the led and olde (display) gets activated 
     along with audio playing for medication alert, once the action is completed it displays the next alarm timing on the display

--- Version2/PCBcode.ino
+++ Version2/PCBcode.ino
@@ -1,121 +1,220 @@
-#include <EEPROM.h>
+//ACTIVATE THE ALARM FOR EVERY 2MIN
+
+/* In the code provided below, oled(display), ldr and reed switch have been interfaced with the esp32 , 
+ where the alarm gets triggered for every 2 min and at the time of alarm the led and olde (display) gets activated 
+ along with audio playing for medication alert, once the action is completed it displays the next alarm timing on the display
+*/
+
 #include "BluetoothSerial.h"
 #include "SoundData.h"
 #include "XT_DAC_Audio.h"
-#include <Arduino.h>  
+#include <Arduino.h>
 #include <Wire.h>
 #include <RTClib.h>
-           
-BluetoothSerial SerialBT;            // Bluetooth Serial object
-RTC_DS3231 rtc;                     
-  
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+BluetoothSerial SerialBT; // Bluetooth Serial object
+RTC_DS3231 rtc;
+
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-const int sensorPin = 13;          // touch sensor 
-const int ledPin = 27;           //  LED pin blue
-const int ledPin1 = 14;         //  LED pin green
-const int ledPin2 = 12;         //  LED pin BLUE
-const int eepromAddress = 0;   // EEPROM address to store the value
+const int ledPin = 14;            //  LED pin green
+const int ledPin1 = 27;          //  LED pin blue
+int reed_switch = 15;           // reed switch pin
+#define lightsensor 36         // ESP32 pin GPIO36 ( VP) connected to light sensor
+#define analogthreshold 600   //threshold for light intensity
 
-XT_Wav_Class ForceWithYou(Force);   // create an object of type XT_Wav_Class that is used by
-                                   // the dac audio class (below), passing wav data as parameter.
+XT_Wav_Class ForceWithYou(Force); // create an object of type XT_Wav_Class that is used by
+                                  // the dac audio class (below), passing wav data as parameter.
 
-XT_DAC_Audio_Class DacAudio(25, 0);  // Create the main player class object.
-                                    // Use GPIO 25, one of the 2 DAC pins and timer 0
+XT_DAC_Audio_Class DacAudio(25, 0); // Create the main player class object.
+                                   // Use GPIO 25, one of the 2 DAC pins and timer 0
 
-uint32_t DemoCounter = 0;       // Just a counter to use in the serial monitor variables will change
-int buttonState = 0;           // variable for reading the pushbutton status
-
-#define lightsensor  36        // ESP32 pin GPIO36 (ADC0) connected to light sensor
-#define analogthreshold  600   //threshold for light intensity
-
-void setup() 
-{ 
- Serial.begin(115200);
- SerialBT.begin("ESP32");    // Initialize Bluetooth with the device name "ESP32"
- pinMode(ledPin,OUTPUT);
- pinMode(ledPin1,OUTPUT);
- pinMode(ledPin2,OUTPUT);
- pinMode(sensorPin, INPUT);
- int storedValue = EEPROM.read(eepromAddress);
- //Serial.print("Stored Value: ");
- //Serial.println(storedValue);
+void setup()
+{
+    Serial.begin(115200);
+    SerialBT.begin("ESP32");      // Initialize Bluetooth with the device name "ESP32"
+    pinMode(ledPin, OUTPUT);
+    pinMode(ledPin1, OUTPUT);
+    pinMode(reed_switch, INPUT);
  
- if (!rtc.begin())
-  {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
+    if (!rtc.begin())
+    {
+        Serial.println("Couldn't find RTC");
+        while (1)
+            ;
+    }
 
-  // Uncomment the following line to set the RTC to the current date and time
-  rtc.adjust(DateTime(__DATE__, __TIME__));
- }
-
-void loop() 
-{ 
- digitalWrite(ledPin, HIGH);    // setting the rgb off initially , rgb common anode
- digitalWrite(ledPin1,HIGH);
- digitalWrite(ledPin2, HIGH); 
- 
- DateTime now = rtc.now();
- int analogValue = analogRead(lightsensor);   // read the value on analog pin
- //Serial.println(analogValue);
- buttonState = digitalRead(sensorPin);  
- 
- if (now.minute() % 2 == 0 && analogValue < analogthreshold && buttonState == LOW) 
- {
-  int valueToStore = 42;
-  EEPROM.write(eepromAddress, valueToStore);
-  EEPROM.commit();
-  digitalWrite(ledPin, HIGH);         //blue led is off
-  digitalWrite(ledPin1, LOW);         // green led is on  
-  Serial.print(now.hour(), DEC);
-  Serial.print(":");
-  Serial.print(now.minute(), DEC);
-  Serial.print(":");
-  Serial.print(now.second(), DEC);
-  Serial.print(":");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.println(" ");
-  Serial.print(now.day(), DEC);
-  Serial.print("-");
-  Serial.print(now.month(), DEC);
-  Serial.print("-");
-  Serial.println(now.year(), DEC);
-  Serial.println("Alarm ON");      // display on serial monitor
-  //delay(1000);
-  SerialBT.println("Alarm ON");     // Send a Bluetooth message
- {sound();}
-  }
-
-if( analogValue > analogthreshold && buttonState == HIGH ) //|| buttonState == HIGH || analogValue > analogthreshold 
- { 
-  digitalWrite(ledPin, LOW);            // blue led is on
-  digitalWrite(ledPin1, HIGH);          //green led is off
-  Serial.print(now.hour(), DEC);
-  Serial.print(":");
-  Serial.print(now.minute(), DEC);
-  Serial.print(":");
-  Serial.print(now.second(), DEC);
-  Serial.print(":");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.println(" ");
-  Serial.print(now.day(), DEC);
-  Serial.print("-");
-  Serial.print(now.month(), DEC);
-  Serial.print("-");
-  Serial.println(now.year(), DEC);
-  Serial.println("Alarm OFF");      // display on serial monitor
-  delay(1000);
-  SerialBT.println("Alarm OFF");   // Send a Bluetooth message
-  delay(59000);
- }
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
+    // Uncomment the following line to set the RTC to the current date and time
+        rtc.adjust(DateTime(__DATE__, __TIME__));
+        
+        display.clearDisplay();
+        display.setTextColor(WHITE);
+        display.setTextSize(2.9);
+        display.setCursor(15,18);
+        display.print("VARIDOSE");
+    
+        DateTime now = rtc.now();
+        
+        display.setTextSize(2);
+        display.setCursor(8,50);
+        display.println(now.day(), DEC);    
+        
+        display.setTextSize(2);
+        display.setCursor(25,50);
+        display.println("/");
+         
+        display.setTextSize(2);
+        display.setCursor(40,50);
+        display.println(now.month(), DEC);
+         
+        display.setTextSize(2);
+        display.setCursor(65,50);
+        display.println("/");
+        
+        display.setTextSize(2);
+        display.setCursor(80,50);
+        display.println(now.year(), DEC);
+        display.display();
+        delay(5000); 
 }
 
+void loop()
+{
+    digitalWrite(ledPin, HIGH);         // setting the rgb off initially , rgb common anode
+    digitalWrite(ledPin1, HIGH);
+
+    DateTime now = rtc.now();
+    int analogValue = analogRead(lightsensor);    
+    int reed_status = digitalRead(reed_switch);
+
+    if (now.minute() % 2 == 0 && reed_status == LOW && analogValue < analogthreshold)
+    {
+        digitalWrite(ledPin, HIGH);          //blue led is off
+        digitalWrite(ledPin1, LOW);         // green led is on
+        
+        Serial.print(now.hour(), DEC);
+        Serial.print(":");
+        Serial.print(now.minute(), DEC);
+        Serial.print(":");
+        Serial.print(now.second(), DEC);
+        Serial.print(":");
+        Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+        Serial.println(" ");
+        Serial.print(now.day(), DEC);
+        Serial.print("-");
+        Serial.print(now.month(), DEC);
+        Serial.print("-");
+        Serial.println(now.year(), DEC);
+        Serial.println("Alarm ON");              // display on serial monitor
+        SerialBT.println("Alarm ON");           // Send a Bluetooth message
+        sound();    
+  
+        display.clearDisplay();
+        display.setTextSize(2);
+        display.setCursor(90,45);
+        display.println(now.second(), DEC);
+       
+        display.setTextSize(2);
+        display.setCursor(75,45);
+        display.println(":");
+         
+        display.setTextSize(2);
+        display.setCursor(50,45);
+        display.println(now.minute(), DEC);
+                
+        display.setTextSize(2);
+        display.setCursor(35,45);
+        display.println(":");
+         
+        display.setTextSize(2);
+        display.setCursor(10,45);
+        display.println(now.hour(), DEC);
+          
+        display.setTextSize(2);
+        display.setCursor(7,0);
+        display.print("Medication");
+        display.display(); 
+        
+        display.setTextSize(2);
+        display.setCursor(30,23);
+        display.print("Alert");
+        display.display(); 
+    }
+  if (now.minute() % 2 != 0 && reed_status == HIGH && analogValue > analogthreshold)     //|| buttonState == HIGH || analogValue > analogthreshold
+    {
+        digitalWrite(ledPin, LOW); // blue led is on
+        digitalWrite(ledPin1, HIGH); //green led is off
+       
+        DateTime now = rtc.now();
+  
+        // Increment the minutes by 2 to display the next alarm
+        now = now + TimeSpan(0, 0, 1  ,0);
+        
+        display.clearDisplay();
+        display.setTextSize(2);
+        display.setCursor(90,45);
+        display.println("00");
+       
+        display.setTextSize(2);
+        display.setCursor(75,45);
+        display.println(":");
+         
+        display.setTextSize(2);
+        display.setCursor(50,45);
+        display.println(now.minute(), DEC);
+                
+        display.setTextSize(2);
+        display.setCursor(35,45);
+        display.println(":");
+         
+        display.setTextSize(2);
+        display.setCursor(10,45);
+        display.println(now.hour(), DEC);
+          
+        display.setTextSize(2);
+        display.setCursor(30,0);
+        display.print("Next");
+        display.display(); 
+        
+        display.setTextSize(2);
+        display.setCursor(30,23);
+        display.print("Alarm");
+        display.display(); 
+          
+        Serial.print(now.hour(), DEC);
+        Serial.print(":");
+        Serial.print(now.minute(), DEC);
+        Serial.print(":");
+        Serial.print(now.second(), DEC);
+        Serial.print(":");
+        Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+        Serial.print(" ");
+        Serial.print(now.day(), DEC);
+        Serial.print("-");
+        Serial.print(now.month(), DEC);
+        Serial.print("-");
+        Serial.println(now.year(), DEC);
+        Serial.println("Alarm OFF");
+        Serial.println("NEXT ALARM");               // display on serial monitor
+        delay(1000);
+        SerialBT.println("Alarm OFF");             // Send a Bluetooth message      
+    }    
+}
 void sound()
 {
-  //delay(20);
-  DacAudio.FillBuffer();                // Fill the sound buffer with data
-  if (ForceWithYou.Playing == false)    // if not playing,
-  DacAudio.Play(&ForceWithYou);         // play it, this will cause it to repeat and repeat...  
+    DacAudio.FillBuffer();             // Fill the sound buffer with data
+    if (ForceWithYou.Playing == false) // if not playing,
+      DacAudio.Play(&ForceWithYou);  // play it, this will cause it to repeat and repeat...
 }
